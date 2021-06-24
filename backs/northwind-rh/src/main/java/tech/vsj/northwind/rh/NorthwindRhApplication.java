8a0bcae6eac3b116c333bd1de280e4d37997caed
Valdir Junior <junior_vs@outlook.com>
2021-06-23 21:12:26 -0300

    setup do projeto

---
+++ backs/northwind-rh/src/main/java/tech/vsj/northwind/rh/NorthwindRhApplication.java
@@ -0,0 +1,13 @@
+package tech.vsj.northwind.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindRhApplication.class, args);
+	}
+
+}
